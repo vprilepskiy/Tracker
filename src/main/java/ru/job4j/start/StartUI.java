c32c50c1c7d303e23ce5abde6accf7863926523e
vprilepskiy <a89122285824@gmail.com>
2016-12-17 11:43:25 +0500

    modify pom.xml

---
+++ src/main/java/ru/job4j/start/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.start;
+
+public class StartUI {
+	
+	/**
+	 * Точка входа в программу.
+	 * @param args - аргумент. 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+	}
+}
